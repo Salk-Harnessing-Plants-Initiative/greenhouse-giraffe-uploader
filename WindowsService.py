4bfe1ea0018e0e44d8d0523da43f15e7205294c6
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2021-01-26 19:19:16 -0800

    Add files via upload

--- WindowsService.py
+++ WindowsService.py
@@ -6,11 +6,14 @@ import win32event
 import win32service
 import win32serviceutil
 
+import subprocess
+PATH_TO_PYTHON = 'C:\\Users\\Greenhouse\\AppData\\Local\\Programs\\Python\\Python36\\python.exe'
+PATH_TO_MAIN = "C:\\Users\\Greenhouse\\Documents\\greenhouse-giraffe-uploader\\main.py"
 
 class TestService(win32serviceutil.ServiceFramework):
-    _svc_name_ = "TestService"
-    _svc_display_name_ = "Test Service"
-    _svc_description_ = "My service description"
+    _svc_name_ = "GiraffeUploaderService"
+    _svc_display_name_ = "Giraffe Uploader Service"
+    _svc_description_ = "Processes and uploads the images to AWS S3"
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
@@ -23,10 +26,16 @@ class TestService(win32serviceutil.ServiceFramework):
 
     def SvcDoRun(self):
         rc = None
-        while rc != win32event.WAIT_OBJECT_0:
-            with open('C:\\TestService.log', 'a') as f:
-                f.write('test service running...\n')
-            rc = win32event.WaitForSingleObject(self.hWaitStop, 5000)
+        with open('C:\\TestService.log', 'a') as f:
+            f.write('hello world...\n')
+            while rc != win32event.WAIT_OBJECT_0:
+                f.write('apple...\n')
+                try:
+                    p = subprocess.run([PATH_TO_PYTHON, PATH_TO_MAIN], stdout=f, stderr=subprocess.STDOUT)
+                except Exception as e:
+                    f.write("ERROR: " + repr(e) + "\n")
+                f.write('banana...\n')
+                
 
 
 if __name__ == '__main__':
